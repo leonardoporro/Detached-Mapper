889e8457ce8379348fe1834195e2fbea51fe3bdb
Leonardo Porro <LeonardoP@cubictelecom.com>
2023-04-30 00:07:30 -0300

    hot chocolate adding package and tests

---
+++ src/Detached.Mappers.HotChocolate/Package.cs
@@ -0,0 +1,13 @@
+﻿using Detached.Mappers.HotChocolate.TypeMappers.POCO.Optional;
+
+namespace Detached.Mappers.HotChocolate
+{
+    public static class Package
+    {
+        public static MapperOptions WithHotChocolate(this MapperOptions mapperOptions)
+        {
+            mapperOptions.TypeMapperFactories.Add(new OptionalTypeMapperFactory());
+            return mapperOptions;
+        }
+    }
+}
