8fa151c45ae59af3d71b3424b0eebc275095b311
theissto <Theiss@systecgmbh.onmicrosoft.com>
2023-05-22 08:20:51 +0200

    Added better description for owned types test case

--- contrib/Detached.Mappers.EntityFramework.Contrib.SysTec/ComplexDeepModelBugReproTests.cs
+++ contrib/Detached.Mappers.EntityFramework.Contrib.SysTec/ComplexDeepModelBugReproTests.cs
@@ -879,6 +879,8 @@ namespace Detached.Mappers.EntityFramework.Contrib.SysTec
             {
                 Age = 16,
                 Name = "Chuck Norris",
+                // This class is marked as owned with the [Owned] attribute
+                // StudentGrades also is a property of the corresponding Student class
                 Grades = new StudentGrades()
                 {
                     English = "A+",
