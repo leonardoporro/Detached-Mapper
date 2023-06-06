4a1354ae9c1005c89bc53ce0c774837c964b90cd
Nico Ullrich <Nico.Ullrich@systec-computer.de>
2023-06-06 15:02:57 +0200

    Add Test 17

---
+++ contrib/Detached.Mappers.EntityFramework.Contrib.SysTec/DTOs/Bug17/PositionBaseDTO.cs
@@ -0,0 +1,8 @@
+﻿using Detached.Mappers.EntityFramework.Contrib.SysTec.ComplexModels;
+
+namespace Detached.Mappers.EntityFramework.Contrib.SysTec.DTOs.Bug17;
+
+public abstract class PositionBaseDTO : IdBaseDTO
+{
+    public string Positionsart { get; set; }
+}
