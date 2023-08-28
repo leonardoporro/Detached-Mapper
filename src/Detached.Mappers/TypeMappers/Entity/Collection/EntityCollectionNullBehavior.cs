f6fdffeb832abfcf702a1fc5605a15d20e3424d8
Michael M <michael@m-bit.nl>
2023-08-28 20:43:47 +0200

    Added missing file for last commit

---
+++ src/Detached.Mappers/TypeMappers/Entity/Collection/EntityCollectionNullBehavior.cs
@@ -0,0 +1,17 @@
+﻿namespace Detached.Mappers.TypeMappers.Entity.Collection
+{
+    /// <summary>
+    /// EntityCollectionTypeMapper mapping behaviour on null collections
+    /// </summary>
+    public enum EntityCollectionNullBehavior
+    {
+        /// <summary>
+        /// Set target to empty collection if source is null
+        /// </summary>
+        SetEmpty,
+        /// <summary>
+        /// Leave target value as-is if source is null
+        /// </summary>
+        Ignore
+    }
+}
