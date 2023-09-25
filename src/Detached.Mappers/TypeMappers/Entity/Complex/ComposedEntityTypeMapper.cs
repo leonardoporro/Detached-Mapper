aa10831d092a07d2867666e87ee7ace5ed4152d8
Leonardo Porro <lporro@qubeyond.com>
2023-09-25 12:08:57 -0300

    refactoring the mapcontext

--- src/Detached.Mappers/TypeMappers/Entity/Complex/ComposedEntityTypeMapper.cs
+++ src/Detached.Mappers/TypeMappers/Entity/Complex/ComposedEntityTypeMapper.cs
@@ -2,29 +2,19 @@
 
 namespace Detached.Mappers.TypeMappers.Entity.Complex
 {
-    public class ComposedEntityTypeMapper<TSource, TTarget, TKey> : TypeMapper<TSource, TTarget>
+    public class ComposedEntityTypeMapper<TSource, TTarget, TKey> : EntityTypeMapper<TSource, TTarget, TKey>
         where TSource : class
         where TTarget : class
         where TKey : IEntityKey
     {
-        readonly Action<TSource, TTarget, IMapContext> _mapKeyMembers;
-        readonly Action<TSource, TTarget, IMapContext> _mapNoKeyMembers;
-        readonly Func<IMapContext, TTarget> _construct;
-        readonly Func<TTarget, IMapContext, TKey> _getTargetKey;
-        readonly Func<TSource, IMapContext, TKey> _getSourceKey;
-
         public ComposedEntityTypeMapper(
             Func<IMapContext, TTarget> construct,
             Func<TSource, IMapContext, TKey> getSourceKey,
             Func<TTarget, IMapContext, TKey> getTargetKey,
             Action<TSource, TTarget, IMapContext> mapKeyMembers,
             Action<TSource, TTarget, IMapContext> mapNoKeyMembers)
+            : base(construct, getSourceKey, getTargetKey, mapKeyMembers, mapNoKeyMembers)
         {
-            _construct = construct;
-            _getSourceKey = getSourceKey;
-            _getTargetKey = getTargetKey;
-            _mapKeyMembers = mapKeyMembers;
-            _mapNoKeyMembers = mapNoKeyMembers;
         }
 
         public override TTarget Map(TSource source, TTarget target, IMapContext context)
@@ -33,14 +23,13 @@ namespace Detached.Mappers.TypeMappers.Entity.Complex
             {
                 if (target != null)
                 {
-                    TKey sourceKey = _getTargetKey(target, context);
-                    context.TrackChange(target, source, sourceKey, MapperActionType.Delete);
-                    target = null;
+                    TKey targetKey = GetTargetKey(target, context);
+                    target = Delete(target, targetKey, context);
                 }
             }
             else
             {
-                TKey sourceKey = _getSourceKey(source, context);
+                TKey sourceKey = GetSourceKey(source, context);
                 EntityRef entityRef = new EntityRef(sourceKey, typeof(TTarget));
 
                 if (context.TryGetResult(entityRef, out TTarget parent))
@@ -49,42 +38,23 @@ namespace Detached.Mappers.TypeMappers.Entity.Complex
                 }
                 else if (target == null)
                 {
-                    target = Create(source, context, sourceKey, entityRef);
+                    target = Create(source, sourceKey, context, entityRef);
                 }
                 else
                 {
-                    TKey targetKey = _getTargetKey(target, context);
-                    bool isOwnedEntityWithoutKey = targetKey is NoKey && sourceKey is NoKey;
-                    //Problem with Owned Entities: sourcekey and targetKey are both NoKey with the same properties, but Equals does not recognize them as equal values
-                    if (Equals(sourceKey, targetKey) || isOwnedEntityWithoutKey)
-                    {
-                        context.PushResult(entityRef, target);
-                        _mapNoKeyMembers(source, target, context);
-                        context.PopResult();
+                    TKey targetKey = GetTargetKey(target, context);
 
-                        target = context.TrackChange(target, source, targetKey, MapperActionType.Update);
+                    if (Equals(sourceKey, targetKey))
+                    {
+                        target = Merge(source, target, targetKey, context, entityRef);
                     }
                     else
                     {
-                        target = Create(source, context, sourceKey, entityRef);
-
-                        context.TrackChange(target, source, targetKey, MapperActionType.Delete);
+                        target = Replace(source, context, sourceKey, entityRef, targetKey);
                     }
                 }
             }
 
-            return target;
-        }
-
-        private TTarget Create(TSource source, IMapContext context, TKey sourceKey, EntityRef entityRef)
-        {
-            TTarget target = _construct(context);
-            _mapKeyMembers(source, target, context);
-            target = context.TrackChange(target, source, sourceKey, MapperActionType.Create);
-
-            context.PushResult(entityRef, target);
-            _mapNoKeyMembers(source, target, context);
-            context.PopResult();
             return target;
         }
     }
---
+++ src/Detached.Mappers/TypeMappers/Entity/Complex/EntityTypeMapper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Detached.Mappers.TypeMappers.Entity.Complex
+{
+    public abstract class EntityTypeMapper<TSource, TTarget, TKey> : TypeMapper<TSource, TTarget>
+        where TSource : class
+        where TTarget : class
+        where TKey : IEntityKey
+    {
+        protected EntityTypeMapper(
+            Func<IMapContext, TTarget> construct,
+            Func<TSource, IMapContext, TKey> getSourceKey,
+            Func<TTarget, IMapContext, TKey> getTargetKey,
+            Action<TSource, TTarget, IMapContext> mapKeyMembers,
+            Action<TSource, TTarget, IMapContext> mapNoKeyMembers)
+        {
+            Construct = construct;
+            GetSourceKey = getSourceKey;
+            GetTargetKey = getTargetKey;
+            MapKeyMembers = mapKeyMembers;
+            MapMembers = mapNoKeyMembers;
+        }
+
+        protected Action<TSource, TTarget, IMapContext> MapKeyMembers { get; }
+
+        protected Action<TSource, TTarget, IMapContext> MapMembers { get; }
+
+        protected Func<IMapContext, TTarget> Construct { get; }
+
+        protected Func<TTarget, IMapContext, TKey> GetTargetKey { get; }
+
+        protected Func<TSource, IMapContext, TKey> GetSourceKey { get; }
+         
+        protected virtual TTarget Create(TSource source, TKey key, IMapContext context, EntityRef entityRef)
+        {
+            TTarget target = Construct(context);
+            MapKeyMembers(source, target, context);
+            target = context.TrackChange(target, source, key, MapperActionType.Create);
+
+            context.PushResult(entityRef, target);
+            MapMembers(source, target, context);
+            context.PopResult();
+            return target;
+        }
+
+        protected virtual TTarget Merge(TSource source, TTarget target, TKey key, IMapContext context, EntityRef entityRef)
+        {
+            context.PushResult(entityRef, target);
+            MapMembers(source, target, context);
+            context.PopResult();
+
+            return context.TrackChange(target, source, key, MapperActionType.Update);
+        }
+
+        protected virtual TTarget Replace(TSource source, IMapContext context, TKey sourceKey, EntityRef entityRef, TKey targetKey)
+        {
+            TTarget target = Create(source, sourceKey, context, entityRef);
+            context.TrackChange(target, source, targetKey, MapperActionType.Delete);
+            return target;
+        }
+
+        protected virtual TTarget Delete(TTarget target, TKey key, IMapContext context)
+        {
+            context.TrackChange<TTarget, TSource, TKey>(target, null, key, MapperActionType.Delete);
+            return null;
+        }
+
+        protected virtual TTarget Attach(TSource source, TKey sourceKey, IMapContext context)
+        {
+            TTarget target = Construct(context);
+            MapKeyMembers(source, target, context);
+            MapMembers(source, target, context); // Only primitives, like Name, Description, etc.
+
+            return context.TrackChange(target, source, sourceKey, MapperActionType.Attach);
+        }
+    }
+}
